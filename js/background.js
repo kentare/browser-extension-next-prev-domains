dc30fbc3fb56220b92a47e6869c0351cb842c4b7
Kent Are <kent@nhi.no>
2022-08-17 10:58:57 +0200

    chore: rm debug console msg

--- js/background.js
+++ js/background.js
@@ -1,8 +1,6 @@
 const myURLs = ["expireddomains.net/domains/"]
 
-console.log("hey")
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  console.log("hey2")
   if (
     changeInfo.status == "complete" &&
     myURLs.some((url) => tab.url.includes(url))
